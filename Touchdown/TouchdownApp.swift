e9d3fecb2cdfb77a3112adfa53666adec93eb484
Elijah Patric <foreverelijah@gmail.com>
2022-11-03 16:56:07 -0500

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Elijah on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
